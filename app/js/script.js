c53fe7790c54ce4796dd59120163993a7df55f00
ehsanmavaei <ehsanmavaei@gmail.com>
2022-04-25 09:27:08 +0430

    finish the design

--- app/js/script.js
+++ app/js/script.js
@@ -3,3 +3,20 @@ console.log('HELLO');
 const test = () => {
 	console.log('this is a test');
 };
+//remove duplicate from numbers
+const numbers=[1,2,3,1,4,5,6,7,7,7,8,1,2]
+
+
+
+let uniqueNumbers = [];
+numbers.forEach((element) => {
+    if (!uniqueNumbers.includes(element)) {
+        uniqueNumbers.push(element);
+    }
+});
+
+let unique = [...new Set(numbers)];
+
+console.log(uniqueNumbers);
+
+console.log(unique);
